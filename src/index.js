0dd5f5152f23e8a7cca67ac2cc592bf891e2b8f1
Лев Оспенников < >
2016-11-08 03:06:27 +0300

    Task2B added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import canonizeName from './canonizeName.js'
+
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
@@ -13,8 +15,15 @@ app.get('/task2A', (req, res) => {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
   const sum = (a || 0) + (b || 0);
-   
+
   res.send(sum.toString());
+});
+
+app.get('/task2B', (req, res) => {
+  const name = req.query.fullname;
+  const canonicalName = canonizeName(name);
+  console.log(canonicalName);
+  res.send(canonicalName);
 });
 
 app.listen(3000, () => {
