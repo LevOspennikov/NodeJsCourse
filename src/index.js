e3878efb8e48825c2b689a2d1b34d6d9d4d2393b
Лев Оспенников < >
2016-11-10 19:16:20 +0300

    Task2C added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-
-import canonizeName from './canonizeName.js'
+import canonizeName from './canonizeName';
+import canonizeNickname from './canonize';
 
 const app = express();
 app.use(cors());
@@ -24,6 +24,14 @@ app.get('/task2B', (req, res) => {
   const canonicalName = canonizeName(name);
   console.log(canonicalName);
   res.send(canonicalName);
+});
+
+
+app.get('/task2C', (req, res) => {
+  const url = req.query.username ;
+  const canonicalName = canonizeNickname(url);
+  console.log(canonicalName);
+  res.send('@' + canonicalName);
 });
 
 app.listen(3000, () => {
