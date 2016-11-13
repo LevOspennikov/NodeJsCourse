a0740a078e2f92cd5f910c0ebebad10817449251
Лев Оспенников < >
2016-11-13 23:00:29 +0300

    3a + 2

--- task2a/src/canonize.js
+++
@@ -1,7 +0,0 @@
-
-export default function canonize(url) {
-  const re = new RegExp('@?(https?:)?(\/\/)?((www\.)?([a-zA-Z-\._])[^\/]*\/)?@?([a-zA-Z_]*[\.]*[a-zA-Z_]*)', 'i');
-  const username = url.match(re);
-  console.log(username);
-  return username[6];
-}
